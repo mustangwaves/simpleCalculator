f08a441e3af413df144c5c55d62fd40baeea67f4
Aaron <119818687+mustangwaves@users.noreply.github.com>
2024-05-19 22:51:53 -0400

    Add files via upload

---
+++ simpleCalculaor.py
@@ -0,0 +1,33 @@
+import math
+
+action = input("What action would you like to do? (Add, Subtract, Multiply, Divide, Exponent, Square Root.)")
+if action == 'Add':
+    number1 = float(input('Enter the first number'))
+    number2 = float(input('Enter the second number'))
+    solution = number1+number2
+    print(solution)
+elif action == 'Subtract':
+    number1 = float(input('Enter the first number'))
+    number2 = float(input('Enter the second number'))
+    solution = number1-number2
+    print(solution)
+elif action == 'Multiply':
+    number1 = float(input('Enter the first number'))
+    number2 = float(input('Enter the second number'))
+    solution = number1*number2
+    print(solution)
+elif action == 'Divide':
+    number1 = float(input("Enter the numerator"))
+    number2 = float(input("Enter the denominator"))
+    solution = number1/number2
+    print(solution)
+elif action == 'Exponent':
+    number1 = float(input('Enter the base number'))
+    number2 = float(input('Enter the exponent'))
+    solution = number1**number2
+elif action == 'Square Root':
+    number1 = float(input('Enter the number'))
+    solution = math.sqrt(number1)
+    print(solution)
+else:
+    print('Error: input cannot be understood')
